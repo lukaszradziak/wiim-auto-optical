00b2b2f2c46d8c0d7c7bf486a83dfe3ade9fa58e
Łukasz Radziak <lukasz.radziak@icloud.com>
2025-01-02 14:34:46 +0100

    Fix delay

--- wiim-auto-optical.js
+++ wiim-auto-optical.js
@@ -1,5 +1,5 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-const WIIM_IP = '192.168.0.130';
+const WIIM_IP = '192.168.0.131';
 
 const checker = async () => {
     const response = await fetch(`https://${WIIM_IP}/httpapi.asp?command=getPlayerStatus`);
@@ -14,7 +14,7 @@ const checker = async () => {
       await fetch(`https://${WIIM_IP}/httpapi.asp?command=setPlayerCmd:switchmode:optical`);
     }
 
-    setTimeout(checker, 15*1000);
+    setTimeout(checker, 10*1000);
 }
 
 checker();
